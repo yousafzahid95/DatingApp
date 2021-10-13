f24fc1f67b103a97c49ae849aaba6cfbe5ee23f1
yousafzahid95 <y.rivaz95@gmail.com>
2021-10-13 10:18:22 +0500

    End of section 9

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -28,11 +28,9 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
         {
-            var users = await _userRepository.GetUsersAsync();
+            var users = await _userRepository.GetMembersAsync();
 
-            var usersToReturn = _mapper.Map<IEnumerable<MemberDTO>>(users);
-            
-            return Ok(usersToReturn);
+            return Ok(users);
         }
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDTO>> GetUser(string username)
